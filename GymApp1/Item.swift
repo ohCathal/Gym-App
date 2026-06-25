ab3b9ff003bab99edbc278008c64e712013e5fa9
Cathal Davitt <c23491404@mytudublin.ie>
2026-06-26 01:31:21 +0900

    Initial Commit

---
+++ GymApp1/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GymApp1
+//
+//  Created by Cathal Davitt on 26/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
